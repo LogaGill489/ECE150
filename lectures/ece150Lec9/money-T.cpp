00b9a8f71b57c87e9ab92ce42aad6db1fa462276
LogaGill489 <logagill489@ed.amdsb.ca>
2024-09-27 14:01:54 -0400

    Created a bit swapper for lec 11

--- lectures/ece150Lec9/money-T.cpp
+++ lectures/ece150Lec9/money-T.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
 #include <cassert>
 
-int main() {
+int main()
+{
     int input;
     std::cout << "Enter an odd number for the money T: ";
     std::cin >> input;
     assert(input % 2 != 0);
     assert(input >= 3);
-    for (int i = 0; i < input; ++i) {
+    for (int i = 0; i < input; ++i)
+    {
         std::cout << "$";
     }
     std::cout << std::endl;
-    for (int i = 0; i < (input - 2); ++i) {
-        for(int j = 0; j < ((input - 1)/2); ++j) {
+    for (int i = 0; i < (input - 2); ++i)
+    {
+        for (int j = 0; j < ((input - 1) / 2); ++j)
+        {
             std::cout << " ";
         }
         std::cout << "$" << std::endl;
