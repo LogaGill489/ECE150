891f847f3f863f7e0b7c270cb6306aac899140e4
LogaGill489 <logagill489@ed.amdsb.ca>
2024-10-31 16:53:17 -0400

    Finished project #3, started project #4

--- projects/project_3/project_3.cpp
+++ projects/project_3/project_3.cpp
@@ -4,10 +4,12 @@ int main();
 
 double *geometric(double a, double ratio, std::size_t cap)
 {
-    double *array = new double [cap]; //declare dynamically allocated array
-    for (int i = 0; i < cap; i++) {
+    double *array = new double[cap]; // declare dynamically allocated array
+    for (int i = 0; i < cap; i++)
+    {
         array[i] = a;
-        for (int k = 0; k < i; k++) {
+        for (int k = 0; k < i; k++)
+        {
             array[i] *= ratio;
         }
     }
@@ -17,16 +19,19 @@ double *geometric(double a, double ratio, std::size_t cap)
 
 double *cross_correlation(double array0[], std::size_t cap0, double array1[], std::size_t cap1)
 {
-    assert( cap0 + cap1 >= 1);
-    double *array = new double [cap0 + cap1 - 1];
+    assert(cap0 + cap1 >= 1);
+    double *array = new double[cap0 + cap1 - 1];
 
-    for (int i = 0; i < (cap0 + cap1 - 1); i++) {
+    for (int i = 0; i < (cap0 + cap1 - 1); i++)
+    {
         array[i] = 0.0;
     }
 
-    for (int i = 0; i < cap0; i++) {
-        for (int j = 0; j < cap1; j++) {
-            array[i + j] += array0[i]*array1[j];
+    for (int i = 0; i < cap0; i++)
+    {
+        for (int j = 0; j < cap1; j++)
+        {
+            array[i + j] += array0[i] * array1[j];
         }
     }
     return array;
@@ -34,14 +39,70 @@ double *cross_correlation(double array0[], std::size_t cap0, double array1[], st
 
 std::size_t shift_duplicates(int array[], std::size_t capacity)
 {
-    return capacity;
+    int flipped{0};
+    int tot_flipped{0};
+    std::size_t uniques{0};
+    bool did_flip = false;
+    for (int i = 0; i < (capacity - flipped); i++)
+    {
+        for (int j = i + 1; j < (capacity - flipped); j++)
+        {
+            if (array[i] == array[j])
+            {
+                int set = array[i];
+                for (int k = j; k < (capacity - 1); k++)
+                {
+                    array[k] = array[k + 1];
+                }
+                array[capacity - 1] = set;
+                flipped++;
+                j--;
+                did_flip = true;
+            }
+        }
+        if (did_flip) {
+            did_flip = false;
+            tot_flipped++;
+        }
+    }
+
+    uniques = capacity - flipped - tot_flipped;
+
+    // prints out the array back for testing
+    /*
+    for (int i = 0; i < 11; i++) {
+        std::cout << array[i] << std::endl;
+    }
+    std::cout << std::endl;
+
+    int k = 0;
+    */
+
+    return uniques;
 }
 
 void deallocate(double *&ptr, bool is_array, std::size_t capacity = 0)
 {
+    if (is_array)
+    {
+        for (int i = 0; i < capacity; i++) {
+            ptr[i] = 0;
+        }
+        delete[] ptr;
+    }
+    else
+    {
+        *ptr = 0;
+        delete ptr;
+    }
+    ptr = nullptr;
 }
 
 int main()
 {
-
+    int array[11] = {0, 13, 2, 13, 4, 13, 89, 5, 4, 7679, 1};
+    int arr2[2] = {0, 0};
+    int arr[1] = {2};
+    std::cout << shift_duplicates(arr2, 2);
+    return 0;
 }
---
+++ projects/project_4/project_4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+int main();
+
+std::size_t length( char const *a ) {
+
+}
+
+int compare( char const *str1, char const *str2 ) {
+
+}
+
+void assign( char *str1, char const *str2 ) {
+
+}
+
+unsigned int distance( char const *str1, char const *str2 ) {
+    
+}
+
+int main() {
+
+}
